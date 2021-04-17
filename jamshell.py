09198751d3e9e917a23f8dbd249fb1ac8ae1bcd2
snidarian <cephalopod31956@gmail.com>
2021-04-17 13:59:32 -0400

    file updated

--- jamshell.py
+++ jamshell.py
@@ -5,22 +5,24 @@ from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
-
+from prompt_toolkit.lexers import PygmentsLexer
+from pygments.lexers.shell import BashLexer
 
 BashCompleter = WordCompleter(['echo', 'ls', 'cat'], ignore_case=True)
 
 
 
-while 1:
-    inp = prompt(u'> ', history=FileHistory('.jamshell_history'), auto_suggest=AutoSuggestFromHistory(), completer=BashCompleter)
-    if inp == "quit" or inp == "exit":
-        break
-    try:
-        os.system(inp)
-    except:
-        print("Invalid Command")
-
-
+try:
+    while 1:
+        inp = prompt(u'--> ', history=FileHistory('.jamshell_history'), auto_suggest=AutoSuggestFromHistory(), completer=BashCompleter, lexer=PygmentsLexer(BashLexer))
+        if inp == "quit" or inp == "exit":
+            break
+        try:
+            os.system(inp)
+        except:
+            print("Invalid Command")
+except:
+    print("Program terminated")
 
 
 
