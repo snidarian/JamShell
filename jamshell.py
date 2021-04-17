35fe680656831b4d2ac6ae9a75aaa86858695f1c
snidarian <cephalopod31956@gmail.com>
2021-04-17 13:25:58 -0400

    functioning shell. Added Autosuggest from histy and wordcompleter

--- jamshell.py
+++ jamshell.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 
-
+import os
 from prompt_toolkit import prompt
-from prompt_toolkit import FileHistory
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.completion import WordCompleter
+
+
+BashCompleter = WordCompleter(['echo', 'ls', 'cat'], ignore_case=True)
+
 
 
 while 1:
-    inp = prompt(u'> ', history=FileHistory('history.txt'))
-    print(inp)
+    inp = prompt(u'> ', history=FileHistory('.jamshell_history'), auto_suggest=AutoSuggestFromHistory(), completer=BashCompleter)
+    if inp == "quit" or inp == "exit":
+        break
+    try:
+        os.system(inp)
+    except:
+        print("Invalid Command")
 
 
 
